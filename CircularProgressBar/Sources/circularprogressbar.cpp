bfcf9328975d9c1f062c71a43a7916c4cfdc1c00
Mariusz <mariusz.baran23@gmail.com>
2015-01-01 22:27:32 +0100

    Type of progressbar

--- CircularProgressBar/Sources/circularprogressbar.cpp
+++ CircularProgressBar/Sources/circularprogressbar.cpp
@@ -1,7 +1,7 @@
 #include "circularprogressbar.h"
 
 
-CircularProgressBar::CircularProgressBar(QWidget *parent) :
+CircularProgressBar::CircularProgressBar(QWidget *parent, TYPE typ) :
     QWidget(parent)
 {
     radius = qMin(this->width()/2, this->height()/2);
@@ -12,17 +12,52 @@ CircularProgressBar::CircularProgressBar(QWidget *parent) :
 void CircularProgressBar::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
-    //painter.setBrush(QBrush(borderColor));
     painter.drawEllipse(center,radius,radius);
     if(m_x < 0){
         m_x=0;
     }
     if(m_x > 100){
         m_x=100;
-    }else{
+    }
+
+
+    switch(m_type){
+    case FILLING:{
         painter.setBrush(QBrush(fillColor));
         painter.drawEllipse(center, radius*m_x/100, radius*m_x/100);
+        break;
     }
+    case ROUNDING:{
+        if(m_x != 0 && m_x != 100){
+            painter.setBrush(QBrush(fillColor));
+            painter.drawLine(center.x(),center.y(),center.x(),center.y()-radius);
+            double val = (double)m_x;
+            double angle = qDegreesToRadians(val/100*360);
+
+            int sinAR = (int) (qSin(angle)*radius);
+            int cosAR = (int) (qCos(angle)*radius);
+
+            if(m_x <= 25){
+                painter.drawLine(center.x(),center.y(),center.x()+sinAR,center.y()-cosAR);
+            }
+            if(m_x > 25 && m_x <= 50){
+                painter.drawLine(center.x(),center.y(),center.x()+sinAR,center.y()-cosAR);
+            }
+            if(m_x > 50 && m_x <= 75){
+                painter.drawLine(center.x(),center.y(),center.x()+sinAR,center.y()-cosAR);
+            }
+            if(m_x > 75 && m_x <= 100){
+                painter.drawLine(center.x(),center.y(),center.x()+sinAR,center.y()-cosAR);
+            }
+        }
+        if(m_x == 100){
+            painter.setBrush(QBrush(fillColor));
+            painter.drawEllipse(center, radius*m_x/100, radius*m_x/100);
+        }
+        break;
+    }
+    }
+
 }
 
 void CircularProgressBar::resizeEvent(QResizeEvent *event)
@@ -48,4 +83,15 @@ void CircularProgressBar::setColor(QColor color)
 {
     fillColor = color;
     repaint();
+}
+
+void CircularProgressBar::setType(CircularProgressBar::TYPE typ)
+{
+    m_type = typ;
+    repaint();
+}
+
+QPoint CircularProgressBar::getCenter() const
+{
+    return center;
 }
